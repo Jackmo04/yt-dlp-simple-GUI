467c28d4e1a62bf7ea78907422ebc744cfc76c47
i7 Parrot <mortix03@gmail.com>
2026-02-09 19:21:33 +0100

    Added tkinter import

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
+import tkinter as tk
+
 class GUI:
     def __init__(self):
         self.controller = None
+        self.root = tk.Tk()
+        # TODO
 
     def set_controller(self, controller):
         self.controller = controller
+        # TODO
 
     def begin(self):
-        pass # TODO
+        self.root.mainloop()
