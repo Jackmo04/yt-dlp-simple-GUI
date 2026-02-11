a247e50f4ae6decfecb3408d93a576d0146f6bde
Matteo Grandini <matteo.grandini@studio.unibo.it>
2026-02-11 17:11:04 +0100

    Added info messages in run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ FIRST_RUN=false
 
 if [[ ! -d ".venv" ]]; then
     FIRST_RUN=true
+    echo "Creating virtual environment..."
     python3 -m venv .venv
 fi
 
 source .venv/bin/activate
 
 if $FIRST_RUN; then
+    echo "Installing dependencies..."
     pip install -r requirements.txt
 fi
 
